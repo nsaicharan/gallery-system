a7e5fecfac62fa0d8e53d0669aa5e09a61f1f39a
Sai <saicharan13@gmail.com>
2018-08-21 16:10:28 +0530

    Create basic structure for admin panel pages

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -4,9 +4,10 @@
     <div class="row">
         <div class="col-lg-12">
             <h1 class="page-header">
-                Blank Page
+                Admin Page
                 <small>Subheading</small>
             </h1>
+
             <ol class="breadcrumb">
                 <li>
                     <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
